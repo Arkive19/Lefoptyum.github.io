bab704a7e1a57876b0915fbf5f643a31e9963c82
Aarambha Pyakurel <arkaive19@gmail.com>
2025-05-23 14:20:10 +0545

    git

--- Scripts/scramblerText.js
+++ Scripts/scramblerText.js
@@ -1,68 +1,64 @@
 class TextScramble {
-
-
   //TODO - idkr
-    constructor(el) {
-      this.el = el
-      this.chars = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz'
-      this.update = this.update.bind(this)
-    }
+  constructor(el) {
+    this.el = el;
+    this.chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+    this.update = this.update.bind(this);
+  }
 
-    setText (newText) {
-      const oldText = this.el.innerText
-      const length = Math.max(oldText.length, newText.length)
-      const promise = new Promise((resolve) => this.resolve = resolve)
-      this.queue = []
-      for (let i = 0; i < length; i++) {
-        const from = oldText[i] || ''
-        const to = newText[i] || ''
-        const start = Math.floor(Math.random() * 20)
-        const end = start + Math.floor(Math.random() * 20)
-        this.queue.push({ from, to, start, end })
-      }
-      cancelAnimationFrame(this.frameRequest)
-      this.frame = 0
-      this.update()
-      return promise
+  setText(newText) {
+    const oldText = this.el.innerText;
+    const length = Math.max(oldText.length, newText.length);
+    const promise = new Promise((resolve) => (this.resolve = resolve));
+    this.queue = [];
+    for (let i = 0; i < length; i++) {
+      const from = oldText[i] || "";
+      const to = newText[i] || "";
+      const start = Math.floor(Math.random() * 20);
+      const end = start + Math.floor(Math.random() * 20);
+      this.queue.push({ from, to, start, end });
     }
-    update() {
-      let output = ''
-      let complete = 0
-      for (let i = 0, n = this.queue.length; i < n; i++) {
-        let { from, to, start, end, char } = this.queue[i]
-        if (this.frame >= end) {
-          complete++
-          output += to
-        } else if (this.frame >= start) {
-          if (!char || Math.random() < 0.28) {
-            char = this.randomChar()
-            this.queue[i].char = char
-          }
-          output += `<span class="dud">${char}</span>`
-        } else {
-          output += from
+    cancelAnimationFrame(this.frameRequest);
+    this.frame = 0;
+    this.update();
+    return promise;
+  }
+  update() {
+    let output = "";
+    let complete = 0;
+    for (let i = 0, n = this.queue.length; i < n; i++) {
+      let { from, to, start, end, char } = this.queue[i];
+      if (this.frame >= end) {
+        complete++;
+        output += to;
+      } else if (this.frame >= start) {
+        if (!char || Math.random() < 0.28) {
+          char = this.randomChar();
+          this.queue[i].char = char;
         }
-      }
-      this.el.innerHTML = output
-      if (complete === this.queue.length) {
-        this.resolve()
+        output += `<span class="dud">${char}</span>`;
       } else {
-        this.frameRequest = requestAnimationFrame(this.update)
-        this.frame++
+        output += from;
       }
     }
-    randomChar() {
-      return this.chars[Math.floor(Math.random() * this.chars.length)]
+    this.el.innerHTML = output;
+    if (complete === this.queue.length) {
+      this.resolve();
+    } else {
+      this.frameRequest = requestAnimationFrame(this.update);
+      this.frame++;
     }
   }
-  
-  const handleScramble = e => {
-    fx.setText("Lefoptyum");
+  randomChar() {
+    return this.chars[Math.floor(Math.random() * this.chars.length)];
   }
-  
-  const el = document.querySelector('.grad')
-  const fx = new TextScramble(el)
-  
-  el.addEventListener("mouseover", handleScramble);
-  
-  
+}
+
+const handleScramble = (e) => {
+  fx.setText("Lefoptyum");
+};
+
+const el = document.querySelector(".grad");
+const fx = new TextScramble(el);
+
+el.addEventListener("mouseover", handleScramble);
