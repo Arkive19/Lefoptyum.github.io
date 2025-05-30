05e5d2d9f9ba0ba7f29206e7765a4972bafe6c3e
Aarambha Pyakurel <arkaive19@gmail.com>
2025-05-30 11:23:42 +0545

    Finishing Touches

--- Scripts/boilerPlate.js
+++ Scripts/boilerPlate.js
@@ -7,14 +7,14 @@ document.addEventListener("DOMContentLoaded", () => {
             </a>
         </div>
         <div class="nav-links" id="nav-links">
-            <a href="./home.html" class="nav nav-btn">Home</a>
+            <a href="./index.html" class="nav nav-btn">Home</a>
             <a href="./projects.html" class="nav nav-btn">Projects</a>
             <a href="./contact.html" class="nav nav-btn">Contact</a>
             <a href="./about-me.html" class="nav nav-btn">About Me</a>
         </div>
         <div id="menu" class="nav-popup-menu">
           <ul>
-            <li><a href="./home.html">Home</a></li>
+            <li><a href="./index.html">Home</a></li>
             <li><a href="./projects.html">Projects</a></li>
             <li><a href="./contact.html">Contact</a></li>
             <li><a href="./about-me.html">About Me</a></li>
@@ -28,12 +28,12 @@ document.addEventListener("DOMContentLoaded", () => {
     const theToggle = document.getElementById("toggle");
     const menu = document.getElementById("menu");
     function toggleMenu() {
-      if (window.innerWidth <= 1134) {
+      if (window.innerWidth <= 1167) {
         menu.classList.toggle("active");
       }
     }
     theToggle.addEventListener("click", function (e) {
-      if (window.innerWidth <= 1134) {
+      if (window.innerWidth <= 1167) {
         e.preventDefault();
         toggleMenu();
       }
@@ -41,14 +41,14 @@ document.addEventListener("DOMContentLoaded", () => {
     // Hide menu when clicking a link (on mobile)
     menu.querySelectorAll("a").forEach((link) => {
       link.addEventListener("click", () => {
-        if (window.innerWidth <= 1134) {
+        if (window.innerWidth <= 1167) {
           menu.classList.remove("active");
         }
       });
     });
-    // Hide menu if window resized above 1134px
+    // Hide menu if window resized above 1167px
     window.addEventListener("resize", () => {
-      if (window.innerWidth > 1134) {
+      if (window.innerWidth > 1167) {
         menu.classList.remove("active");
       }
     });
